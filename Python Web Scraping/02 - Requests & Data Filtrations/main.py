27693f588c630da710d402f9126a49ec7ae1325d
nathan-pham <157486459+nathan-pham@users.noreply.github.com>
2024-12-28 16:15:23 +1100

    Change to iteration over jobs

--- Python Web Scraping/02 - Requests & Data Filtrations/main.py
+++ Python Web Scraping/02 - Requests & Data Filtrations/main.py
@@ -3,12 +3,16 @@ import requests
 
 html_text = requests.get('https://www.timesjobs.com/candidate/job-search.html?searchType=personalizedSearch&from=submit&searchTextSrc=&searchTextText=&txtKeywords=python&txtLocation=').text
 soup = BeautifulSoup(html_text, 'lxml')
-job = soup.find('li', class_= 'clearfix job-bx wht-shd-bx')
-company_name = job.find('h3', class_ = 'joblist-comp-name').text.replace('\t', '').replace(' ', ''). replace('\n', '')
-skills = job.find('div', class_ = 'srp-skills').text.replace('\t', '').replace(' ', '').replace('\n\n', '').replace('\n', ' ')
-published_date = job.find('span', class_ = 'sim-posted').span.text
+jobs = soup.find_all('li', class_= 'clearfix job-bx wht-shd-bx')
 
-#print(f'''
-#Company Name: {company_name}
-#Required Skills: {skills}
-#''')
+for job in jobs:
+    company_name = job.find('h3', class_ = 'joblist-comp-name').text.replace('\t', '').replace(' ', ''). replace('\n', '')
+    skills = job.find('div', class_ = 'srp-skills').text.replace('\t', '').replace(' ', '').replace('\n\n', '').replace('\n', ' ')
+    published_date = job.find('span', class_ = 'sim-posted').span.text
+
+    print(f'''
+    Company Name: {company_name}
+    Required Skills: {skills}
+    ''')
+
+    print('')
