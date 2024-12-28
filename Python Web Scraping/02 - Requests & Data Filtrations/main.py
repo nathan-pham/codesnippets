b7e69ab4d4bc597ab4cf951e4c872e10bb612126
nathan-pham <157486459+nathan-pham@users.noreply.github.com>
2024-12-28 16:00:26 +1100

    Extract company name from job

--- Python Web Scraping/02 - Requests & Data Filtrations/main.py
+++ Python Web Scraping/02 - Requests & Data Filtrations/main.py
@@ -4,4 +4,5 @@ import requests
 html_text = requests.get('https://www.timesjobs.com/candidate/job-search.html?searchType=personalizedSearch&from=submit&searchTextSrc=&searchTextText=&txtKeywords=python&txtLocation=').text
 soup = BeautifulSoup(html_text, 'lxml')
 job = soup.find('li', class_= 'clearfix job-bx wht-shd-bx')
-print(job)
+company_name = job.find('h3', class_ = 'joblist-comp-name').text
+print(company_name)
