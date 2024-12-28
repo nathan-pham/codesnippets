4b870b387bd054d5291a0b9144113e5bf9eaccc8
nathan-pham <157486459+nathan-pham@users.noreply.github.com>
2024-12-28 15:27:48 +1100

    Print h5 tags from home.html

--- Python Web Scraping/01 - Scraping Basics/main.py
+++ Python Web Scraping/01 - Scraping Basics/main.py
@@ -2,7 +2,7 @@ from bs4 import BeautifulSoup
 
 with open('home.html', 'r') as html_file:
     content = html_file.read()
-    print(content)
 
     soup = BeautifulSoup(content, 'lxml')
-    print(soup.prettify())
+    tags = soup.find('h5')
+    print(tags)
