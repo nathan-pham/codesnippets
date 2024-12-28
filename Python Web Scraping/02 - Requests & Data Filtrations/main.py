0363a0a6dc1ed4aaa55c0a6f2f6a8615b530be6b
nathan-pham <157486459+nathan-pham@users.noreply.github.com>
2024-12-28 16:04:06 +1100

    Format company name

--- Python Web Scraping/02 - Requests & Data Filtrations/main.py
+++ Python Web Scraping/02 - Requests & Data Filtrations/main.py
@@ -4,5 +4,5 @@ import requests
 html_text = requests.get('https://www.timesjobs.com/candidate/job-search.html?searchType=personalizedSearch&from=submit&searchTextSrc=&searchTextText=&txtKeywords=python&txtLocation=').text
 soup = BeautifulSoup(html_text, 'lxml')
 job = soup.find('li', class_= 'clearfix job-bx wht-shd-bx')
-company_name = job.find('h3', class_ = 'joblist-comp-name').text
+company_name = job.find('h3', class_ = 'joblist-comp-name').text.replace('\t', '').replace(' ', '')
 print(company_name)
