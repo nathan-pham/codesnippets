f4b6f9416114f49464462988442e3665465d2dc3
nathan-pham <157486459+nathan-pham@users.noreply.github.com>
2024-12-28 15:59:24 +1100

    Find one job

--- Python Web Scraping/02 - Requests & Data Filtrations/main.py
+++ Python Web Scraping/02 - Requests & Data Filtrations/main.py
@@ -1,4 +1,7 @@
 from bs4 import BeautifulSoup
 import requests
 
-html_text = requests.get('')
+html_text = requests.get('https://www.timesjobs.com/candidate/job-search.html?searchType=personalizedSearch&from=submit&searchTextSrc=&searchTextText=&txtKeywords=python&txtLocation=').text
+soup = BeautifulSoup(html_text, 'lxml')
+job = soup.find('li', class_= 'clearfix job-bx wht-shd-bx')
+print(job)
