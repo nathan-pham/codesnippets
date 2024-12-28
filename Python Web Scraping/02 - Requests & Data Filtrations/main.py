b5da0e3ebe385c66eb0ab997f5efb632dbcf793f
nathan-pham <157486459+nathan-pham@users.noreply.github.com>
2024-12-28 16:13:31 +1100

    Extract published date

--- Python Web Scraping/02 - Requests & Data Filtrations/main.py
+++ Python Web Scraping/02 - Requests & Data Filtrations/main.py
@@ -6,8 +6,9 @@ soup = BeautifulSoup(html_text, 'lxml')
 job = soup.find('li', class_= 'clearfix job-bx wht-shd-bx')
 company_name = job.find('h3', class_ = 'joblist-comp-name').text.replace('\t', '').replace(' ', ''). replace('\n', '')
 skills = job.find('div', class_ = 'srp-skills').text.replace('\t', '').replace(' ', '').replace('\n\n', '').replace('\n', ' ')
+published_date = job.find('span', class_ = 'sim-posted').span.text
 
-print(f'''
-Company Name: {company_name}
-Required Skills: {skills}
-''')
+#print(f'''
+#Company Name: {company_name}
+#Required Skills: {skills}
+#''')
