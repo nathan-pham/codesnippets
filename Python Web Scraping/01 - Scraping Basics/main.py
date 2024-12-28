0a156e0e0171d89e148428f9ca3435f05bf44b74
nathan-pham <157486459+nathan-pham@users.noreply.github.com>
2024-12-28 15:40:05 +1100

    Format course print statements

--- Python Web Scraping/01 - Scraping Basics/main.py
+++ Python Web Scraping/01 - Scraping Basics/main.py
@@ -7,7 +7,5 @@ with open('home.html', 'r') as html_file:
     course_cards = soup.find_all('div', class_ = 'card')
     for course in course_cards:
         course_name = course.h5.text
-        course_price = course.a.text
-
-        print(course_name)
-        print(course_price)
+        course_price = course.a.text.split()[-1]
+        print(f"{course_name} costs {course_price}")
