6d8712992221fe30358206690ef9c03b49708cdf
nathan-pham <157486459+nathan-pham@users.noreply.github.com>
2024-12-28 16:09:46 +1100

    Print job information

--- Python Web Scraping/02 - Requests & Data Filtrations/main.py
+++ Python Web Scraping/02 - Requests & Data Filtrations/main.py
@@ -4,7 +4,10 @@ import requests
 html_text = requests.get('https://www.timesjobs.com/candidate/job-search.html?searchType=personalizedSearch&from=submit&searchTextSrc=&searchTextText=&txtKeywords=python&txtLocation=').text
 soup = BeautifulSoup(html_text, 'lxml')
 job = soup.find('li', class_= 'clearfix job-bx wht-shd-bx')
-company_name = job.find('h3', class_ = 'joblist-comp-name').text.replace('\t', '').replace(' ', '')
+company_name = job.find('h3', class_ = 'joblist-comp-name').text.replace('\t', '').replace(' ', ''). replace('\n', '')
 skills = job.find('div', class_ = 'srp-skills').text.replace('\t', '').replace(' ', '').replace('\n\n', '').replace('\n', ' ')
 
-print(skills)
+print(f'''
+Company Name: {company_name}
+Required Skills: {skills}
+''')
