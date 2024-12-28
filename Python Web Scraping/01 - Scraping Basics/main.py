d01cba080494b6020629c811362dc91f0eb6848d
nathan-pham <157486459+nathan-pham@users.noreply.github.com>
2024-12-28 15:28:55 +1100

    Find all h5 tags from home.html

--- Python Web Scraping/01 - Scraping Basics/main.py
+++ Python Web Scraping/01 - Scraping Basics/main.py
@@ -4,5 +4,5 @@ with open('home.html', 'r') as html_file:
     content = html_file.read()
 
     soup = BeautifulSoup(content, 'lxml')
-    tags = soup.find('h5')
+    tags = soup.find_all('h5')
     print(tags)
