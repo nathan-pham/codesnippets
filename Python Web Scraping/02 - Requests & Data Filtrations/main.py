4c9929942d680c411e14114bb8e0d4ac3a0d0f09
nathan-pham <157486459+nathan-pham@users.noreply.github.com>
2024-12-28 16:18:16 +1100

    Add recency filter to code

--- Python Web Scraping/02 - Requests & Data Filtrations/main.py
+++ Python Web Scraping/02 - Requests & Data Filtrations/main.py
@@ -6,13 +6,16 @@ soup = BeautifulSoup(html_text, 'lxml')
 jobs = soup.find_all('li', class_= 'clearfix job-bx wht-shd-bx')
 
 for job in jobs:
-    company_name = job.find('h3', class_ = 'joblist-comp-name').text.replace('\t', '').replace(' ', ''). replace('\n', '')
-    skills = job.find('div', class_ = 'srp-skills').text.replace('\t', '').replace(' ', '').replace('\n\n', '').replace('\n', ' ')
+
     published_date = job.find('span', class_ = 'sim-posted').span.text
 
-    print(f'''
+    if 'few' in published_date:
+        company_name = job.find('h3', class_ = 'joblist-comp-name').text.replace('\t', '').replace(' ', ''). replace('\n', '')
+        skills = job.find('div', class_ = 'srp-skills').text.replace('\t', '').replace(' ', '').replace('\n\n', '').replace('\n', ' ')
+    
+        print(f'''
 Company Name: {company_name}
 Required Skills: {skills}
-    ''')
+        ''')
 
-    print('')
+        print('')
