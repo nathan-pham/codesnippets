079a985809decccab636cd4492431ae35422caa3
nathan-pham <157486459+nathan-pham@users.noreply.github.com>
2024-12-28 15:36:27 +1100

    Print all courses

--- Python Web Scraping/01 - Scraping Basics/main.py
+++ Python Web Scraping/01 - Scraping Basics/main.py
@@ -4,6 +4,6 @@ with open('home.html', 'r') as html_file:
     content = html_file.read()
 
     soup = BeautifulSoup(content, 'lxml')
-    courses_html_tags = soup.find_all('h5')
-    for course in courses_html_tags:
-        print(course.text)
+    course_cards = soup.find_all('div', class_ = 'card')
+    for course in course_cards:
+        print(course)
