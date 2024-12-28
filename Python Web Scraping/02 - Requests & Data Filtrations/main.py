6c3c304b5a876f974dda7587ee1e472444df28bf
nathan-pham <157486459+nathan-pham@users.noreply.github.com>
2024-12-28 16:22:24 +1100

    Change formatting of output

--- Python Web Scraping/02 - Requests & Data Filtrations/main.py
+++ Python Web Scraping/02 - Requests & Data Filtrations/main.py
@@ -10,12 +10,10 @@ for job in jobs:
     published_date = job.find('span', class_ = 'sim-posted').span.text
 
     if 'few' in published_date:
-        company_name = job.find('h3', class_ = 'joblist-comp-name').text.replace('\t', '').replace(' ', ''). replace('\n', '')
+        company_name = job.find('h3', class_ = 'joblist-comp-name').text.replace(' ', '')
         skills = job.find('div', class_ = 'srp-skills').text.replace('\t', '').replace(' ', '').replace('\n\n', '').replace('\n', ' ')
-    
-        print(f'''
-Company Name: {company_name}
-Required Skills: {skills}
-        ''')
+
+        print(f"Company Name: {company_name.strip()}")
+        print(f"Required Skills: {skills}")
 
         print('')
