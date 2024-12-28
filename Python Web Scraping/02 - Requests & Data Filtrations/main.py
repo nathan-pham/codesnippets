676a983cb4639e8a45bb30bf4342aacef552781b
nathan-pham <157486459+nathan-pham@users.noreply.github.com>
2024-12-28 16:31:26 +1100

    Abstract code into find_jobs()

--- Python Web Scraping/02 - Requests & Data Filtrations/main.py
+++ Python Web Scraping/02 - Requests & Data Filtrations/main.py
@@ -5,22 +5,23 @@ print('Put some skills that you are not familiar with')
 unfamiliar_skill = input('>').lower()
 print(f'Filtering out {unfamiliar_skill}')
 
-html_text = requests.get('https://www.timesjobs.com/candidate/job-search.html?searchType=personalizedSearch&from=submit&searchTextSrc=&searchTextText=&txtKeywords=python&txtLocation=').text
-soup = BeautifulSoup(html_text, 'lxml')
-jobs = soup.find_all('li', class_= 'clearfix job-bx wht-shd-bx')
+def find_jobs():
+    html_text = requests.get('https://www.timesjobs.com/candidate/job-search.html?searchType=personalizedSearch&from=submit&searchTextSrc=&searchTextText=&txtKeywords=python&txtLocation=').text
+    soup = BeautifulSoup(html_text, 'lxml')
+    jobs = soup.find_all('li', class_= 'clearfix job-bx wht-shd-bx')
 
-for job in jobs:
+    for job in jobs:
 
-    published_date = job.find('span', class_ = 'sim-posted').span.text
+        published_date = job.find('span', class_ = 'sim-posted').span.text
 
-    if 'few' in published_date:
-        company_name = job.find('h3', class_ = 'joblist-comp-name').text.replace(' ', '')
-        skills = job.find('div', class_ = 'srp-skills').text.replace('\t', '').replace(' ', '').replace('\n\n', '').replace('\n', ' ').lower()
-        more_info = job.header.h2.a['href']
+        if 'few' in published_date:
+            company_name = job.find('h3', class_ = 'joblist-comp-name').text.replace(' ', '')
+            skills = job.find('div', class_ = 'srp-skills').text.replace('\t', '').replace(' ', '').replace('\n\n', '').replace('\n', ' ').lower()
+            more_info = job.header.h2.a['href']
 
-        if unfamiliar_skill not in skills:
-            print(f"Company Name: {company_name.strip()}")
-            print(f"Required Skills: {skills}")
-            print(f"More Info: {more_info}")
+            if unfamiliar_skill not in skills:
+                print(f"Company Name: {company_name.strip()}")
+                print(f"Required Skills: {skills}")
+                print(f"More Info: {more_info}")
 
-        print('')
+            print('')
