b6eb67c3305bd09ed7e50369445edb864b431dec
nathan-pham <157486459+nathan-pham@users.noreply.github.com>
2024-12-28 16:25:54 +1100

    Add link scraping functionality

--- Python Web Scraping/02 - Requests & Data Filtrations/main.py
+++ Python Web Scraping/02 - Requests & Data Filtrations/main.py
@@ -12,8 +12,10 @@ for job in jobs:
     if 'few' in published_date:
         company_name = job.find('h3', class_ = 'joblist-comp-name').text.replace(' ', '')
         skills = job.find('div', class_ = 'srp-skills').text.replace('\t', '').replace(' ', '').replace('\n\n', '').replace('\n', ' ')
+        more_info = job.header.h2.a['href']
 
         print(f"Company Name: {company_name.strip()}")
         print(f"Required Skills: {skills}")
+        print(f"More Info: {more_info}")
 
         print('')
